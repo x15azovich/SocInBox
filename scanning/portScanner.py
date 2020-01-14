54bbe0b03b6692b0b5c298e9c96f42eb121edef4
jcrunjie <chenrunjie2002@gmail.com>
2020-01-13 21:33:47 -0500

    finished portscanner with banner grabber

--- scanning/portScanner.py
+++ scanning/portScanner.py
@@ -1,14 +1,44 @@
-import socket
+import socket, threading
 socket.setdefaulttimeout(2)
 
 host = input("ip:")
 
-def portscanner(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    if not(s.connect_ex((host,port))):
-        print ("port", port, "is open!!!!!!")
-    s.close()
+def TCP_connect(ip, port_number, delay, output):
+	TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	TCPsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	TCPsock.settimeout(delay)
+	try:
+		TCPsock.connect((ip, port_number))
+		output[port_number] = TCPsock.recv(1024)
+	except:
+		output[port_number] = ''
 
-for port in range (20,26):
-    portscanner(port)
+def scan_ports(host_ip, delay):
+	
+	threads = []        # To run TCP_connect concurrently
+	output = {}         # For printing purposes
+
+    # Spawning threads to scan ports
+	for i in range(10000):
+		t = threading.Thread(target=TCP_connect, args=(host_ip, i, delay, output))
+		threads.append(t)
+
+    # Starting threads
+	for i in range(10000):
+		threads[i].start()
+
+    # Locking the main thread until all threads complete
+	for i in range(10000):
+		threads[i].join()
+
+    # Printing listening ports from small to large
+	for i in range(10000):
+		if output[i] == '':
+			#print("no data found for " + str(i))
+			continue
+		else:
+			print(str(i) + ': ' + str(output[i]))
+
+scan_ports(host, 2)
+
 
