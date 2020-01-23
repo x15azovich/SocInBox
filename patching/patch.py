b329bebdc8eb1fa9678f6573fb8c49ea5b1f5731
jcrunjie <chenrunjie2002@gmail.com>
2020-01-22 19:50:11 -0500

    grabbed port number and message from banner

--- patching/patch.py
+++ patching/patch.py
@@ -1,6 +1,14 @@
+import re
+
 f = open("/root/SocInBox/scanning/portscandata.txt", "r")
 
 while True:
-	x = f.readline()
-	if not x: break
-	print (x)
+	#reading each line of the file
+	line = f.readline()
+
+	#break if no more line
+	if not line: break
+
+	#using regex to grab port number and banner 	
+	x = re.findall(r'([\d]+): b[\'\"](.*)[\'\"]', line)[0]
+	print (x[0] + " " + x[1])
