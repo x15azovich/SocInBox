880985c6d3168cb18b6445e5137ea47345430d1d
x15azovich <azovich@citadel.edu>
2020-03-14 18:04:10 -0400

    Fix py script

--- hostbase/InstallClam.py
+++ hostbase/InstallClam.py
@@ -3,23 +3,26 @@ import os
 
 def operatingSystem(OS, path):
 	if OS=="Windows":
-		path = "~/SocInBox/hostbase/windows7"
-		execute(path)
+		path = "~/SocInBox/hostbase/windows7/ClaimAV-0.1.2.1.exe"
+		executeWindows(path)
 	if OS=="mac":
-                path = "~/SocInBox/hostbase/mac"
-                execute(path)
+                path = "~/SocInBox/hostbase/mac/clamav-0.102.2.tar.gz"
+                executeLinux(path)
 	if OS=="Linux":
-                path = "~/SocInBox/hostbase/Linux"
-                execute(path)
+                path = "~/SocInBox/hostbase/Linux/clamav-0.102.2.tar.gz"
+                executeLinux(path)
 
 
 		
-def execute(path):
-	execute = path + "ClamAV-0.102.1.exe"
+def executeLinux(path):
+#system commands to unpack and install on linux/mac
+	execute = path
 	os.system(execute)
-	print("Seccessfully executed " + execute)
 	
-
+def executeWindows(path):
+	execute = path
+	os.system(execute)
+	
 def main():
 	operating=input("Enter your operating system : ")
 	print ("main ran" + operating)
