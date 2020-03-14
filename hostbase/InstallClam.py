e6e684a37821b6359e207ebbb5737c819ce96c2a
Anthony Zovich <azovich@citadel.edu>
2020-03-14 14:35:46 -0700

    Added linux and mac folders

--- hostbase/InstallClam.py
+++ hostbase/InstallClam.py
@@ -5,6 +5,14 @@ def operatingSystem(OS, path):
 	if OS=="Windows":
 		path = "~/SocInBox/hostbase/windows7"
 		execute(path)
+	if OS=="mac":
+                path = "~/SocInBox/hostbase/mac"
+                execute(path)
+	if OS=="Linux":
+                path = "~/SocInBox/hostbase/Linux"
+                execute(path)
+
+
 		
 def execute(path):
 	execute = path + "ClamAV-0.102.1.exe"
