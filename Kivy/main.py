113f9145441a5960abaca193c75830a02f1b3756
x15azovich <you@example.com>
2020-05-11 21:12:53 -0400

    added text box formatting

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,33 +1,38 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.graphics import Color, Line, Ellipse
+from kivy.uix.floatlayout import FloatLayout
 
 class MainWindow(Screen):
-    pass
+	pass
+
+class HomeWindow(Screen):
+	pass
 
 class PatchingWindow(Screen):
-    pass
+	pass
 
 class NetworkWindow(Screen):
-    pass
+	pass
 
 class ScanningWindow(Screen):
-    pass
+	pass
 
 class HostbaseWindow(Screen):
-    pass
+	pass
 
 class WindowManager(ScreenManager):
-    pass
+	pass
 
 
 kv = Builder.load_file("my.kv")
 
 
 class MyMainApp(App):
-    def build(self):
-        return kv
+	def build(self):
+		return kv
 
 
 if __name__ == "__main__":
-    MyMainApp().run()
+	MyMainApp().run()
