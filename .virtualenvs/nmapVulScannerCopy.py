af470560896fcab22a487cf9366da1221c5c1f4f
x15azovich <you@example.com>
2020-06-04 21:04:36 -0400

    Anthony's Edits

--- .virtualenvs/nmapVulScannerCopy.py
+++ .virtualenvs/nmapVulScannerCopy.py
@@ -1,13 +1,13 @@
 import os, re
 
+ip="192.168.1.153"
 def scan(ip):
-    command = "nmap -sV --script=vulscan/vulscan.nse "+str(ip)+" > portscandata.txt"
+    command = "nmap -sV --script=vulscan/vulscan.nse "+str(ip)+" > portscandata2.txt"
     os.system(command)
 
     pattern = "https://(.*?)\|"
 
-    with open("portscandata.txt", "r") as file:
+    with open("portscandata2.txt", "r") as file:
         for line in file:
             substring = re.search(pattern, line)[0]
             print(substring)
-        
