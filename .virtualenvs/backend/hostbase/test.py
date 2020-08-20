4a6e16b76bb71a45c8d94a93eb21d932c513feb6
unknown <chenrunjie2002@gmail.com>
2020-08-19 22:19:25 -0400

    tested clamAV, now just need to harness the output from powershell correctedly

--- .virtualenvs/backend/hostbase/test.py
+++ .virtualenvs/backend/hostbase/test.py
@@ -1,1 +1,7 @@
-print("test")
+# -*- coding: iso-8859-1 -*-
+import subprocess, sys
+
+p = subprocess.Popen(["powershell.exe", 
+               "cd C:\Users\Jessi\Downloads\clamav-0.102.1-win-x64-portable;.\clamscan"], 
+              stdout=sys.stdout)
+p.communicate()
