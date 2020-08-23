9daa0b649320864001dd52a8cf439e884bb616ce
unknown <chenrunjie2002@gmail.com>
2020-08-22 21:02:12 -0400

    made my first branch yay!!! also got clamav to work

--- .virtualenvs/backend/hostbase/test.py
+++ .virtualenvs/backend/hostbase/test.py
@@ -2,6 +2,21 @@
 import subprocess, sys
 
 p = subprocess.Popen(["powershell.exe", 
-               "cd C:\Users\Jessi\Downloads\clamav-0.102.1-win-x64-portable;.\clamscan"], 
+               "cd C:/Users/Jessi/Downloads/clamav-0.102.1-win-x64-portable;.\clamscan"], 
               stdout=sys.stdout)
 p.communicate()
+
+'''
+import os, re
+
+print("running hostbase scan")
+#this command works for jeff's machine, which is Windows. Need to test if it works for other windows machine too
+WinCommand = "cd C:\Users\Jessi\Downloads\clamav-0.102.1-win-x64-portable"
+WinCommand2 = ".\clamscan > portscandata.txt"
+
+#change which command variable to run depending on which OS you are using
+os.system(WinCommand)
+os.system(WinCommand2)
+
+print("finished scan")
+'''
