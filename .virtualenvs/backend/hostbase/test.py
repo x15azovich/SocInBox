dd8527b704ace8386b14e05e9d3a1ece00a82fb9
unknown <chenrunjie2002@gmail.com>
2020-09-14 21:19:07 -0400

    some one test this pls

--- .virtualenvs/backend/hostbase/test.py
+++ .virtualenvs/backend/hostbase/test.py
@@ -3,6 +3,7 @@ pattern = re.compile("Scanned files: (\d{1,})")
 
 
 for i, line in enumerate(open('clamAVresults.txt')):
+    line = "".join(line.split())
     for match in re.finditer(pattern, line):
         print ('Found on line %s: %s' % (i+1, match.group()))
 
