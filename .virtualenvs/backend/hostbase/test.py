02bbfe1f335902ca9e7fa3a963afcac1bdbf1a7d
unknown <chenrunjie2002@gmail.com>
2020-09-14 21:34:48 -0400

    no work

--- .virtualenvs/backend/hostbase/test.py
+++ .virtualenvs/backend/hostbase/test.py
@@ -1,13 +1,28 @@
 import re
-pattern = re.compile("Scanned files: (\d{1,})")
+pattern = re.compile(" S c a n n e d  f i l e s :  (.*)")
 
+f = open("clamAVresults.txt", "r")
+x = []
+while True:
+    line = f.readline()
+    #print(line)
+    if not line: break
+    line = "".join(line.split())
+    print(line)
+    x += re.findall(r' S c a n n e d   f i l e s :   (.*)', line) 
+    
+for i in x:
+    print(i + '\n')
+
+f.close()
+
+'''
 
 for i, line in enumerate(open('clamAVresults.txt')):
     line = "".join(line.split())
     for match in re.finditer(pattern, line):
-        print ('Found on line %s: %s' % (i+1, match.group()))
+        print ('Found on line %s: %s' % (i+1, match.group(1)))
 
-'''
 with open ('clamAVresults.txt', 'r') as f:
     while True:
         line = f.readline()
@@ -24,7 +39,6 @@ for i, line in enumerate(open('clamAVresults.txt')):
         scannedDir = match.group()
         print(scannedDir)
 
-     
 
 file1 = open('clamAVresults.txt', 'r') 
 Lines = file1.readlines() 
@@ -35,4 +49,5 @@ for line in Lines:
    # line = line.rstrip()
     print("".join(line.split()))
 
-file1.close()   '''
+file1.close()   
+     '''
