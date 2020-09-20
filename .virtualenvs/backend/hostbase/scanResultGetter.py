180d4b57efd56e48409ed56bf270506804ae7f3f
unknown <chenrunjie2002@gmail.com>
2020-09-20 19:49:37 -0400

    finally fixed stuff the right way

---
+++ .virtualenvs/backend/hostbase/scanResultGetter.py
@@ -0,0 +1,23 @@
+import re
+#pattern = re.compile("Scanned files: (\d{0,})")
+def scanResultGetter():
+    scannedfiles = []
+    scannedDir = []
+    infected = []
+    with open ("clamAVresultsCMD.txt", "r") as r:
+        while True:
+            line = r.readline()
+            if not line: break
+            scannedfiles += re.findall(r'Scanned files: (\d{0,})', line)
+            scannedDir += re.findall(r'Scanned directories: (\d{0,})', line)
+            infected += re.findall(r'Infected files: (\d{0,})', line)
+
+    fileScanned = scannedfiles[0]
+    dirScanned = scannedDir[0]
+    filesInfected = infected[0]
+
+    print(fileScanned)
+    print(dirScanned)
+    print(filesInfected)
+
+    return fileScanned, dirScanned, filesInfected
--- .virtualenvs/backend/hostbase/test.py
+++ .virtualenvs/backend/hostbase/test.py
@@ -1,50 +1,21 @@
 import re
-pattern = re.compile(" S c a n n e d  f i l e s :  (.*)")
+#pattern = re.compile("Scanned files: (\d{0,})")
 
-f = open("clamAVresults.txt", "r")
-x = []
-while True:
-    line = f.readline()
-    #print(line)
-    if not line: break
-    x += re.findall(r'S.*c.*a.*n.*.*n.*e.*d.*f.*i.*l.*e.*s.*:(.*)', line)
-for i in x:
-    print(i + '\n')
-
-f.close()
-
-'''
-
-for i, line in enumerate(open('clamAVresults.txt')):
-    line = "".join(line.split())
-    for match in re.finditer(pattern, line):
-        print ('Found on line %s: %s' % (i+1, match.group(1)))
-
-with open ('clamAVresults.txt', 'r') as f:
+scannedfiles = []
+scannedDir = []
+infected = []
+with open ("clamAVresultsCMD.txt", "r") as r:
     while True:
-        line = f.readline()
+        line = r.readline()
         if not line: break
-        linelol = "".join(line.split()).replace(" ", "")
-        print(linelol)
-        print(re.findall(r'Scanned\sfiles:\s(\d{1,})', linelol)) 
+        scannedfiles += re.findall(r'Scanned files: (\d{0,})', line)
+        scannedDir += re.findall(r'Scanned directories: (\d{0,})', line)
+        infected += re.findall(r'Infected files: (\d{0,})', line)
 
+fileScanned = scannedfiles[0]
+dirScanned = scannedDir[0]
+filesInfected = infected[0]
 
-
-for i, line in enumerate(open('clamAVresults.txt')):
-    print(line)
-    for match in re.finditer(pattern, line):
-        scannedDir = match.group()
-        print(scannedDir)
-
-
-file1 = open('clamAVresults.txt', 'r') 
-Lines = file1.readlines() 
-#print(Lines[1]) 
-count = 0
-# Strips the newline character 
-for line in Lines:
-   # line = line.rstrip()
-    print("".join(line.split()))
-
-file1.close()   
-     '''
+print(fileScanned)
+print(dirScanned)
+print(filesInfected)
