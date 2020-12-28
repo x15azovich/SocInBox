e31ac8cbdb94c59a89e1b5189bc93835e689c61a
x15azovich <azovich@citadel.edu>
2020-12-28 18:24:05 -0500

    Jessi did this not Anthony

--- .virtualenvs/backend/hostbase/hostBaseScan.py
+++ .virtualenvs/backend/hostbase/hostBaseScan.py
@@ -1,19 +1,47 @@
-#returns (in the same order listed here): fileScanned, dirScanned, filesInfected, userDir
-#userDir is the user's entered directory for scanning, use this information to access the clamAVresults.txt like this: {userDir}/clamAVresults.txt
-
 import subprocess, sys, re, os
 
-def hostBaseScan(userDir):
+#takes in userDir, which is the desired folder path to be scanned
+#returns scan and cleanning result if applicable
+def scan(userDir):
     print("running hostbase scan")
     #userDir = "C:\\Users\\Jessi\Desktop\\ScanTest" #example directory for testing purposes
-    filePath = f"{userDir}\\hostbaseScanResults.txt"
-    command = f'cmd /c "cd %ProgramFiles%\Windows Defender & .\MpCmdRun.exe -Scan -ScanType 3 -File {userDir} > {filePath}"'
+    filePath = f"{userDir}\\hostbaseScanResults"
+    command = f'cmd /c "cd %ProgramFiles%\Windows Defender & .\MpCmdRun.exe -Scan -ScanType 3 -File {userDir} > {filePath + ".txt"}"'
     os.system(command)
     result = []
-    with open (filePath, "r") as r:
+    with open (filePath + ".txt", "r") as r:
         while True:
             line = r.readline()
             if not line: break
             result += re.findall(r'found (.*) threats', line)
     print("finished scan")
     return "Found " + result[0] + " threat(s)"
+
+#takes in OutputfilePath, which is the desired path to store the quarantined file path results, which will be a file called quarantinedFiles.txt
+#returns quarantined file names in string format
+def listQuarantine(OutputfilePath):
+    #command to list all quarantined file
+    command2 = f'cmd /c "cd %ProgramFiles%\Windows Defender & .\MpCmdRun.exe -Restore -ListAll > {OutputfilePath + "quarantinedFiles.txt"}"'
+    print(os.system(command2))
+    
+    result = ""
+    with open (OutputfilePath + "quarantinedFiles.txt", "r") as r:
+        while True:
+            line = r.readline()
+            if not line: break
+            result += line
+    return result
+#takes in OutputfilePath, which is the desired path to store the past detection results, in a file called pastDetections.txt
+#returns all detected file paths (include ones that has been deleted as well) in a csv formatted string
+def getPastDetections(OutputfilePath):
+    subprocess.call(f'%SystemRoot%/system32/WindowsPowerShell/v1.0/powershell.exe Get-MpThreatDetection > {OutputfilePath + "pastDetections.txt"}', shell=True)
+    result = ""
+    pattern = re.compile(r'file:_(.*)[,|}][\n| ]')
+    with open (OutputfilePath + "pastDetections.txt", "r") as r:
+        while True:
+            line = r.readline()
+            if not line: break
+            mo = pattern.search(line)
+            if mo is not None:
+                result += (mo.group(1)) + ", "
+    return result
---
+++ .virtualenvs/backend/hostbase/test.py
@@ -0,0 +1,5 @@
+import hostBaseScan
+userDir = "C:\\Users\\Anthony\\Documents\\Jessi"
+print("scan result: " + hostBaseScan.scan(userDir))
+print("all quarantined files: " + hostBaseScan.listQuarantine(userDir))
+print("all past detections: " + hostBaseScan.getPastDetections(userDir))
